7935a093c815451a711232d3113afd90aab25f69
imdaano <danisanchezpsc@gmail.com>
2025-01-17 13:21:59 +0100

    add new button to show notes

--- js/index.js
+++ js/index.js
@@ -1,34 +1,49 @@
-document.getElementById('scroll-podcast').addEventListener('click', function () {
-    const targetPodcast = document.querySelector('#podcast');
+// document.getElementById('scroll-podcast').addEventListener('click', function () {
+//     const targetPodcast = document.querySelector('#podcast');
 
 
-    if (targetPodcast) {
-        targetPodcast.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
+//     if (targetPodcast) {
+//         targetPodcast.scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'start'
+//         });
+//     }
 
+// });
+
+
+// document.getElementById('scroll-episodios').addEventListener('click', function () {
+//     const targetEpisodios = document.querySelector('#episodios');
+
+//     if (targetEpisodios) {
+//         targetEpisodios.scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'start'
+//         });
+//     }
+// });
+
+const toggleButton = document.getElementById('toggleButton');
+const notes = document.getElementById('notes');
+
+toggleButton.addEventListener('click', () => {
+  // Alterna entre mostrar y ocultar el div con las notas
+  if (notes.style.display === 'none') {
+    notes.style.display = 'block'; // Muestra las notas y ocupa espacio
+    toggleButton.textContent = 'Ocultar Notas'; // Cambia el texto del botón
+  } else {
+    notes.style.display = 'none'; // Oculta las notas y no ocupa espacio
+    toggleButton.textContent = 'Ver Notas'; // Cambia el texto del botón
+  }
 });
 
 
-document.getElementById('scroll-episodios').addEventListener('click', function () {
-    const targetEpisodios = document.querySelector('#episodios');
+// function showHide(){
+//     let container = document.querySelector('.notesHidden');
 
-    if (targetEpisodios) {
-        targetEpisodios.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
-});
-
-function showHide(){
-    let container = document.querySelector('.notesHidden');
-
-    if(container.classList.contains('visible')){
-        container.classList.remove('visible');
-    } else {
-        container.classList.add('visible');
-    }
-}
+//     if(container.classList.contains('visible')){
+//         container.classList.remove('visible');
+//     } else {
+//         container.classList.add('visible');
+//     }
+// }
